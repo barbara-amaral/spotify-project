9c98c14a75d79d3580e51f027de47dd66e3d7e3e
barbara <barbara.souza.amaral@gmail.com>
2024-05-22 01:45:20 -0300

    Criando sidebar-component e bottom-bar component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, SidebarComponent, BottomBarComponent],
 })
 export class AppComponent {
   title = 'spotify-project';
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,88 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+})
+export class SidebarComponent {
+  folders = [
+    {
+      src: '../../assets/liked-songs.png',
+      alt: 'liked songs folder',
+      index: 1,
+      text: 'Músicas curtidas',
+      songsQuantity: '1.273 músicas',
+      playlist: false,
+    },
+    {
+      src: '../../assets/episodes.png',
+      alt: 'episodes folder',
+      index: 2,
+      text: 'Seus episódios',
+      episodesInformation: 'Episódios salvos e baixados',
+      playlist: false,
+    },
+    {
+      src: '../../assets/capa-1.png',
+      alt: 'episodes folder',
+      index: 2,
+      text: 'FUNK PUTARIA 🔞🥵 MANDELAO PESADO DE SP MAIO 2024🔥',
+      episodesInformation: 'fer',
+      playlist: true,
+    },
+    {
+      src: '../../assets/capa-2.png',
+      alt: 'episodes folder',
+      index: 2,
+      text: 'sz',
+      episodesInformation: 'Bárbara Souza',
+      playlist: true,
+    },
+    {
+      src: '../../assets/capa-3.png',
+      alt: 'episodes folder',
+      index: 2,
+      text: 'Heavy Metal Classics (Top 100)',
+      episodesInformation: 'Misi',
+      playlist: true,
+    },
+    {
+      src: '../../assets/capa-4.png',
+      alt: 'episodes folder',
+      index: 2,
+      text: 'PAGODE DA LUD 💜 | ludmilla pagode numanice | toda maliciosa gostosa | ludmila pagode | lud numanice',
+      episodesInformation: '@bussola',
+      playlist: true,
+    },
+    {
+      src: '../../assets/capa-5.png',
+      alt: 'episodes folder',
+      index: 2,
+      text: 'Simple Plan: The Complete Collection',
+      episodesInformation: 'Simple Plan',
+      playlist: true,
+    },
+    {
+      src: '../../assets/capa-6.png',
+      alt: 'episodes folder',
+      index: 2,
+      text: 'This is Pink Floyd',
+      episodesInformation: 'Spotify',
+      playlist: true,
+    },
+    {
+      src: '../../assets/capa-7.png',
+      alt: 'episodes folder',
+      index: 2,
+      text: 'Metal Essentials',
+      episodesInformation: 'Spotify',
+      playlist: true,
+    },
+  ];
+
+  showDropdown: number | null = null;
+}
