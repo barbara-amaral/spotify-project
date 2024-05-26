772d7358f62091b6eb061e597044f08c30198da0
barbara <barbara.souza.amaral@gmail.com>
2024-05-26 14:06:15 -0300

    adicionando animação de progresso ao apertar botão de play.

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { BottomBarComponent } from '../bottom-bar/bottom-bar.component';
 
 @Component({
   selector: 'app-progress-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, BottomBarComponent],
   templateUrl: './progress-bar.component.html',
-  styleUrl: './progress-bar.component.css'
+  styleUrl: './progress-bar.component.css',
 })
 export class ProgressBarComponent {
-
+  @Input()
+  isPlaying: boolean = false;
 }
