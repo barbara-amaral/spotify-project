fa5b4b52cedfae88a2faa89884e0541e60e69147
barbara <barbara.souza.amaral@gmail.com>
2024-05-26 13:30:05 -0300

    Adicionando lógica para mudar ícone do botao ao clicar (pausar e tocar música)

--- src/app/bottom-bar/bottom-bar.component.ts
+++ src/app/bottom-bar/bottom-bar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bottom-bar',
   standalone: true,
   templateUrl: './bottom-bar.component.html',
   styleUrl: './bottom-bar.component.css',
-  imports: [ProgressBarComponent],
+  imports: [ProgressBarComponent, CommonModule],
 })
-export class BottomBarComponent {}
+export class BottomBarComponent {
+  play: boolean = false;
+
+  toggle() {
+    this.play = !this.play;
+  }
+}
